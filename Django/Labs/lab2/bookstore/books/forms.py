9ffb9b62139e4e58649ae7b24de2d88f1f397fae
sondos said <58375277+Sondos11@users.noreply.github.com>
2023-06-18 13:58:21 +0300

    Delete Django/Labs/lab2/bookstore directory

--- Django/Labs/lab2/bookstore/books/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import book
-
-
-class bookForm(ModelForm):
-    class Meta:
-        model = book
-        # fields = '__all__'
-        fields = ['title', 'description', 'rate', 'views']
